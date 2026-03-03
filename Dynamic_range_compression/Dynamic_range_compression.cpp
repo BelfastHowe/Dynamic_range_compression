ae5fefd7b9570611a6eb7ea40129c9f60346864e
LAPTOP-EM3EIUNV\Belfast <1301231026@qq.com>
2026-03-03 14:17:19 +0800

    添加项目文件。

---
+++ Dynamic_range_compression/Dynamic_range_compression.cpp
@@ -0,0 +1,12 @@
+﻿// Dynamic_range_compression.cpp: 定义应用程序的入口点。
+//
+
+#include "Dynamic_range_compression.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
